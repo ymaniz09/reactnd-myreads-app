3581e87652b8089c111f38398a939e46b7ef3409
Ygor <ygor.maniz@gmail.com>
2020-06-27 22:47:55 -0300

    Creating Search and ListBooks components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {Route} from "react-router-dom";
+import ListBooks from "./ListBooks";
+import Search from "./Search";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class BooksApp extends React.Component {
+  state = {
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+    showSearchPage: false
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Route exact path='/' render={() => (
+          <ListBooks>
+
+          </ListBooks>
+        )}/>
+
+        <Route path='/search' render={({}) => (
+          <Search>
+
+          </Search>
+        )}/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default BooksApp
